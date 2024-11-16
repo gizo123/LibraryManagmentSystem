2dcfef01caaca7bddc67d0ab6b347134f290d5a4
Gizo <atom.damage777@gmail.com>
2024-11-16 15:41:11 +0400

    Added Classes

---
+++ LibraryManagmentSystem.App/Objects/Book/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagmentSystem.App.Objects.Book
+{
+    public class Book
+    {
+      
+    }
+}
